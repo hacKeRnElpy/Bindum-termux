c0e6ff44ce4291d4e7e95df1c3b5d64ee75274e7
hacKeRnElpy <realaipaii@gmail.com>
2020-07-14 13:41:09 +0300

    v1.0

---
+++ Bindum.py
@@ -0,0 +1,15 @@
+import os
+while True:
+	print('[1]Create bind')
+	choose=input('\n  [bindum] >> ')
+	if (choose=='1'):
+		bind=input('Input bind: ')
+		bindcommand=input('Input bind command: ')
+		postrov="'"
+		bid='alias ' + bind +'=' + postrov + bindcommand + postrov
+		binds=open('/data/data/com.termux/files/usr/etc/binds.txt', 'a')
+		binds.write('\n' + bid)
+		binds.close
+		f=open('/data/data/com.termux/files/usr/etc/bash.bashrc', 'a')
+		f.write('\n' + bid)
+		f.close
